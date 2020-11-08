d1f1747e7c8b6a2e7af1ec283c4d9ff64e4aae8f
Leonardo Lewandowski <leolewan@gmail.com>
2020-11-08 00:16:31 -0300

    Some refactoring on Card component

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 
 import Placeholder from '../placeholder/placeholder';
 
-const Card = (props) => {
+const Card = ({ onSelect, id, thumbnail, name, age }) => {
   return (
-    <article className="card" onClick={() => props.onSelect(props.id)}>
+    <article className="card" onClick={() => onSelect(id)}>
       <div className="card__header">
         <Placeholder />
-        <img className="card__image" src={props.thumbnail} />
+        <img className="card__image" src={thumbnail} />
       </div>
       <div className="card__content">
-        <small className="card__detail">Id: {props.id}</small>
-        <p className="card__name">{props.name}</p>
-        <small className="card__detail">Age: {props.age}</small>
+        <small className="card__detail">Id: {id}</small>
+        <p className="card__name">{name}</p>
+        <small className="card__detail">Age: {age}</small>
       </div>
     </article>
   );
